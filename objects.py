2b0adde3d9eed97292f84bc8554563bdb5c82915
Saŭlo Rubusarbaro <61021296+rubusarbaro@users.noreply.github.com>
2026-06-17 09:05:06 -0600

    Create objects.py

---
+++ objects.py
@@ -0,0 +1,25 @@
+class Entity():
+    def __init__(self):
+        pass
+
+class Company(Entity):
+    def __init__(self):
+        pass
+
+class Consumer(Entity):
+    def __init__(self):
+        pass
+
+class Manufacturer(Entity):
+    def __init__(self):
+        pass
+
+class Transport(Entity):
+    def __init__(self):
+        pass
+
+class Transport:
+    self.air : None
+    self.motor : None
+    self.rail : None
+    self.sea : None
